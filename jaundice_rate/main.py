e5a8449601a68da62fa1144aa0b777c0820d3bab
mayosen <gare577@gmail.com>
2022-12-14 16:47:13 +0300

    Handle client errors

--- jaundice_rate/main.py
+++ jaundice_rate/main.py
@@ -1,5 +1,7 @@
 import asyncio
 from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 import aiohttp
 import anyio
@@ -16,21 +18,30 @@ async def fetch(session: aiohttp.ClientSession, url: str):
         return await response.text()
 
 
+class ProcessingStatus(Enum):
+    OK = "OK"
+    FETCH_ERROR = "FETCH_ERROR"
+
+
 @dataclass
 class ProcessedArticle:
     url: str
-    rating: float
-    words: int
+    status: ProcessingStatus
+    rating: Optional[float]
+    words: Optional[int]
 
 
 async def process_article(session: aiohttp.ClientSession, url: str, result_list: list[ProcessedArticle]):
-    html = await fetch(session, url)
-    sanitizer = SANITIZERS.get("inosmi_ru")
-    plaintext = sanitizer(html, plaintext=True)
-    morph = pymorphy2.MorphAnalyzer()
-    article_words = split_by_words(morph, plaintext)
-    rate = calculate_jaundice_rate(article_words, CHARGED_WORDS)
-    result_list.append(ProcessedArticle(url, rate, len(article_words)))
+    try:
+        html = await fetch(session, url)
+        sanitizer = SANITIZERS.get("inosmi_ru")
+        plaintext = sanitizer(html, plaintext=True)
+        morph = pymorphy2.MorphAnalyzer()
+        article_words = split_by_words(morph, plaintext)
+        rate = calculate_jaundice_rate(article_words, CHARGED_WORDS)
+        result_list.append(ProcessedArticle(url, ProcessingStatus.OK, rate, len(article_words)))
+    except aiohttp.ClientError:
+        result_list.append(ProcessedArticle(url, ProcessingStatus.FETCH_ERROR, None, None))
 
 
 async def main():
@@ -39,7 +50,8 @@ async def main():
         "https://inosmi.ru/20221214/kitay-258839981.html",
         "https://inosmi.ru/20221214/ultrapravye-258844791.html",
         "https://inosmi.ru/20221214/kitay-258839420.html",
-        "https://inosmi.ru/20221214/katargeyt-258839069.html"
+        "https://inosmi.ru/20221214/katargeyt-258839069.html",
+        "https://inosmi.ru/not/exist.html",
     ]
 
     async with aiohttp.ClientSession() as session:
@@ -50,7 +62,12 @@ async def main():
                 tg.start_soon(process_article, session, url, processed_articles)
 
         for article in processed_articles:
-            print(f"URL: {article.url}\nРейтинг: {article.rating}%\nСлов в статье: {article.words}\n")
+            print(
+                f"URL: {article.url}\n"
+                f"Статус: {article.status.value}\n"
+                f"Рейтинг: {article.rating}\n"
+                f"Слов в статье: {article.words}\n"
+            )
 
 
 if __name__ == "__main__":
